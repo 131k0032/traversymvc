13107dc9111b67a4240f82d1ee73e7e01b8085c6
Trainer <mitnick117@gmail.com>
2021-01-19 17:28:26 -0500

    Archivo config.php y carga automatica de librerias

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,13 @@
 <?php 
+// Archivo config.php
+require_once 'config/config.php';
+
 // Cargando librerias
-require_once 'libraries/Controller.php';
-require_once 'libraries/Core.php';
-require_once 'libraries/Database.php';
+// require_once 'libraries/Controller.php';
+// require_once 'libraries/Core.php';
+// require_once 'libraries/Database.php';
+
+// Carga automatica de librerias
+spl_autoload_register(function ($className){
+	require_once 'libraries/'.$className.'.php';
+});
---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php 
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+// Url root
+define('URLROOT','http://localhost/traversymvc');
+// Site session_name()
+define('SITENAME', 'Traversymvc');
